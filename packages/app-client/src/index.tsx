d517af45b1a7782d6c114b2dc78de94a995f7bd7
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-23 20:33:42 +0200

    feat(ui): default on system color mode

--- packages/app-client/src/index.tsx
+++ packages/app-client/src/index.tsx
@@ -5,25 +5,34 @@ import './app.css';
 
 import { Suspense, render } from 'solid-js/web';
 import { Router } from '@solidjs/router';
-import { ColorModeProvider, ColorModeScript } from '@kobalte/core/color-mode';
+import { ColorModeProvider, ColorModeScript, createLocalStorageManager } from '@kobalte/core/color-mode';
 import { routes } from './routes';
 import { NoteContextProvider } from './modules/notes/notes.context';
 
 render(
-  () => (
-    <Router
-      children={routes}
-      root={props => (
-        <Suspense>
-          <NoteContextProvider>
-            <ColorModeScript storageType="localStorage" storageKey="color_mode" initialColorMode="system" />
-            <ColorModeProvider>
-              <div class="min-h-screen font-sans text-sm font-400">{props.children}</div>
-            </ColorModeProvider>
-          </NoteContextProvider>
-        </Suspense>
-      )}
-    />
-  ),
+  () => {
+    const initialColorMode = 'system';
+    const colorModeStorageKey = 'enclosed_color_mode';
+    const localStorageManager = createLocalStorageManager(colorModeStorageKey);
+
+    return (
+      <Router
+        children={routes}
+        root={props => (
+          <Suspense>
+            <NoteContextProvider>
+              <ColorModeScript storageType={localStorageManager.type} storageKey={colorModeStorageKey} initialColorMode={initialColorMode} />
+              <ColorModeProvider
+                initialColorMode={initialColorMode}
+                storageManager={localStorageManager}
+              >
+                <div class="min-h-screen font-sans text-sm font-400">{props.children}</div>
+              </ColorModeProvider>
+            </NoteContextProvider>
+          </Suspense>
+        )}
+      />
+    );
+  },
   document.getElementById('root')!,
 );
