3a6010929f6a65864b51907fde7331c10db84edd
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-23 20:32:51 +0200

    refactor(routes): mutualised node id regex

--- packages/app-client/src/routes.tsx
+++ packages/app-client/src/routes.tsx
@@ -1,7 +1,9 @@
-import type { RouteDefinition } from '@solidjs/router';
+import { A, type RouteDefinition } from '@solidjs/router';
 import { AppLayout } from './modules/ui/layouts/AppLayout';
 import { CreateNotePage } from './modules/notes/pages/create-note.page';
 import { ViewNotePage } from './modules/notes/pages/view-note.page';
+import { Button } from './modules/ui/components/button';
+import { NOTE_ID_REGEX } from './modules/notes/notes.constants';
 
 export const routes: RouteDefinition[] = [
   {
@@ -15,6 +17,24 @@ export const routes: RouteDefinition[] = [
       {
         path: '/:noteId',
         component: ViewNotePage,
+        matchFilters: {
+          noteId: NOTE_ID_REGEX,
+        },
+      },
+      {
+        path: '*404',
+        component: () => (
+          <div class="flex flex-col items-center justify-center mt-6">
+            <div class="text-3xl font-light text-muted-foreground">404</div>
+            <h1 class="font-semibold text-lg my-2">Page Not Found</h1>
+            <p class="text-muted-foreground">The page you are looking for does not exist.</p>
+            <p class="text-muted-foreground">Please check the URL and try again.</p>
+            <Button as={A} href="/" class="mt-4" variant="secondary">
+              <div class="i-tabler-arrow-left mr-2"></div>
+              Go back home
+            </Button>
+          </div>
+        ),
       },
     ],
   },
