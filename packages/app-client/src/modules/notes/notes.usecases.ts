a7acceb1715806a4d5858e0e1f3f6de36fcd3304
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-24 03:15:06 +0200

    test(int): added encryption/decryption tests

--- packages/app-client/src/modules/notes/notes.usecases.ts
+++ packages/app-client/src/modules/notes/notes.usecases.ts
@@ -3,9 +3,33 @@ import { createRandomBuffer } from '../shared/random/random';
 import { createNoteUrl, decryptNoteContent, deriveMasterKey, encryptNoteContent } from './notes.models';
 import { createNote } from './notes.services';
 
-export { encryptAndCreateNote, decryptNote };
+export { encryptAndCreateNote, decryptNote, encryptNote };
 
-async function encryptAndCreateNote({ content, password, ttlInSeconds, deleteAfterReading }: { content: string; password?: string; ttlInSeconds: number; deleteAfterReading: boolean }) {
+async function encryptAndCreateNote({
+  content,
+  password,
+  ttlInSeconds,
+  deleteAfterReading,
+  storeNote = createNote,
+}: {
+  content: string;
+  password?: string;
+  ttlInSeconds: number;
+  deleteAfterReading: boolean;
+  storeNote?: (params: { content: string; isPasswordProtected: boolean; ttlInSeconds: number; deleteAfterReading: boolean }) => Promise<{ noteId: string }>;
+}) {
+  const { encryptedContent, encryptionKey } = await encryptNote({ content, password });
+
+  // Send the encrypted note to the server for storage, the server has no knowledge of the encryption key
+  const { noteId } = await storeNote({ content: encryptedContent, isPasswordProtected: Boolean(password), ttlInSeconds, deleteAfterReading });
+
+  // The base key is stored in the URL hash fragment
+  const { noteUrl } = createNoteUrl({ noteId, encryptionKey });
+
+  return { encryptedContent, noteId, encryptionKey, noteUrl };
+}
+
+async function encryptNote({ content, password }: { content: string; password?: string }) {
   // The base key ensure e2e encryption even if the user does not provide a password
   const baseKey = createRandomBuffer({ length: 32 });
 
@@ -14,15 +38,9 @@ async function encryptAndCreateNote({ content, password, ttlInSeconds, deleteAft
 
   const encryptedContent = await encryptNoteContent({ content, masterKey });
 
-  // Send the encrypted note to the server for storage
-  const { noteId } = await createNote({ content: encryptedContent, isPasswordProtected: Boolean(password), ttlInSeconds, deleteAfterReading });
-
-  // The base key is stored in the URL hash fragment
   const encryptionKey = bufferToBase64Url({ buffer: baseKey });
 
-  const { noteUrl } = createNoteUrl({ noteId, encryptionKey });
-
-  return { encryptedContent, noteId, encryptionKey, noteUrl };
+  return { encryptedContent, encryptionKey };
 }
 
 async function decryptNote({ encryptedContent, password, encryptionKey }: { encryptedContent: string; password?: string; encryptionKey: string }) {
