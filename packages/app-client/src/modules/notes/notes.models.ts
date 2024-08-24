6e19c590d4e284ff58c1be7cbcd42b21422dc8d2
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2024-08-24 02:51:47 +0200

    refactor(e2ee): use PBKDF2 to generate master key (#6)

--- packages/app-client/src/modules/notes/notes.models.ts
+++ packages/app-client/src/modules/notes/notes.models.ts
@@ -1,10 +1,14 @@
 import { createBuffer, mergeBuffers } from '../shared/crypto/buffer';
-import { aesDecrypt, aesEncrypt } from '../shared/crypto/encryption';
+import { aesDecrypt, aesEncrypt, deriveKey } from '../shared/crypto/encryption';
 import { createSha256Hash } from '../shared/crypto/hash';
 
-export { createBufferFromPassword, getEncryptionKeyHash, encryptNoteContent, decryptNoteContent, createNoteUrl };
+export { createBufferFromPassword, getEncryptionKeyHash, encryptNoteContent, decryptNoteContent, createNoteUrl, deriveMasterKey };
 
-function createBufferFromPassword({ password }: { password: string }) {
+function createBufferFromPassword({ password }: { password?: string }) {
+  if (!password) {
+    return new Uint8Array(0);
+  }
+
   return new TextEncoder().encode(password);
 }
 
@@ -28,26 +32,31 @@ async function getEncryptionKeyHash({
   return encryptionKey;
 }
 
-async function encryptNoteContent({ content, encryptionHashBuffer }: { content: string; encryptionHashBuffer: Uint8Array }) {
+async function encryptNoteContent({ content, masterKey }: { content: string; masterKey: Uint8Array }) {
   const contentBuffer = createBuffer({ value: content });
-  const encryptedContent = await aesEncrypt({ data: contentBuffer, key: encryptionHashBuffer });
+  const encryptedContent = await aesEncrypt({ data: contentBuffer, key: masterKey });
 
   return encryptedContent;
 }
 
-async function decryptNoteContent({ encryptedContent, decryptionHashBuffer }: { encryptedContent: string; decryptionHashBuffer: Uint8Array }) {
-  const decryptedContent = await aesDecrypt({ data: encryptedContent, key: decryptionHashBuffer });
+async function decryptNoteContent({ encryptedContent, masterKey }: { encryptedContent: string; masterKey: Uint8Array }) {
+  const decryptedContent = await aesDecrypt({ data: encryptedContent, key: masterKey });
 
   return decryptedContent;
 }
 
 function createNoteUrl({ noteId, encryptionKey }: { noteId: string; encryptionKey: string }): { noteUrl: string } {
   const url = new URL(`/${noteId}`, window.location.origin);
-  // add the encryption key as hash fragment
-
   url.hash = encryptionKey;
 
   const noteUrl = url.toString();
 
   return { noteUrl };
+}
+
+function deriveMasterKey({ baseKey, password }: { baseKey: Uint8Array; password?: string }) {
+  const passwordBuffer = createBufferFromPassword({ password });
+  const mergedBuffers = mergeBuffers(baseKey, passwordBuffer);
+
+  return deriveKey({ key: mergedBuffers });
 }
--- packages/app-client/src/modules/shared/crypto/encryption.ts
+++ packages/app-client/src/modules/shared/crypto/encryption.ts
@@ -1,6 +1,6 @@
 import { base64UrlToBuffer, bufferToBase64Url, createRandomBuffer } from './buffer';
 
-export { aesEncrypt, aesDecrypt };
+export { aesEncrypt, aesDecrypt, deriveKey };
 
 async function aesEncrypt({ data, key: rawKey }: { data: Uint8Array; key: Uint8Array }): Promise<string> {
   const iv = createRandomBuffer({ length: 12 });
@@ -28,4 +28,29 @@ async function aesDecrypt({ data, key: rawKey }: { data: string; key: Uint8Array
   const decrypted = await crypto.subtle.decrypt({ name: 'AES-GCM', iv }, key, encrypted);
 
   return new TextDecoder().decode(decrypted);
+}
+
+async function deriveKey({ key: originalKey, iterations = 100_000 }: { key: Uint8Array; iterations?: number }): Promise<Uint8Array> {
+  // The salt is deterministically derived from the key
+  const saltHash = await crypto.subtle.digest('SHA-256', originalKey);
+  const salt = new Uint8Array(saltHash).slice(0, 16);
+
+  const key = await crypto.subtle.importKey('raw', originalKey, 'PBKDF2', false, ['deriveKey']);
+
+  const derivedKey = await crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations,
+      hash: 'SHA-256',
+    },
+    key,
+    { name: 'AES-GCM', length: 256 },
+    true, // Extractable
+    ['encrypt', 'decrypt'],
+  );
+
+  const exportedKey = await crypto.subtle.exportKey('raw', derivedKey);
+
+  return new Uint8Array(exportedKey);
 }
