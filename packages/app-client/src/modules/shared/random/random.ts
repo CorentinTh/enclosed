5d86a7807840f34c61b4cc93510ae4bda7f8f8aa
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-25 23:00:40 +0200

    refactor(random): mutualized random buffer creation for string

--- packages/app-client/src/modules/shared/random/random.ts
+++ packages/app-client/src/modules/shared/random/random.ts
@@ -2,16 +2,15 @@ import { bufferToBase64Url } from '../crypto/buffer';
 
 export { createRandomString, createRandomBuffer };
 
-function createRandomString({ length = 16 }: { length?: number } = {}): string {
-  const bufferLength = Math.ceil((length * 3) / 4);
-  const randomValues = new Uint8Array(bufferLength);
-  crypto.getRandomValues(randomValues);
-  return bufferToBase64Url({ buffer: randomValues });
-}
-
 function createRandomBuffer({ length = 16 }: { length?: number } = {}): Uint8Array {
   const randomValues = new Uint8Array(length);
   crypto.getRandomValues(randomValues);
 
   return randomValues;
+}
+
+function createRandomString({ length = 16 }: { length?: number } = {}): string {
+  const bufferLength = Math.ceil((length * 3) / 4);
+  const randomValues = createRandomBuffer({ length: bufferLength });
+  return bufferToBase64Url({ buffer: randomValues });
 }
