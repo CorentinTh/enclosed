790c56188fc3bf940404fde93041b4b9ba957420
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-28 21:28:37 +0200

    chore(deps): removed solid-devtools

--- packages/app-client/vite.config.ts
+++ packages/app-client/vite.config.ts
@@ -2,15 +2,9 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import unoCssPlugin from 'unocss/vite';
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
-    /*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
     unoCssPlugin(),
     solidPlugin(),
   ],
