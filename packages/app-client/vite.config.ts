545717ef88b1849bebeadf9153ee5ffb30256a64
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-08-31 21:04:51 +0200

    feat(dev): added api proxy on vite dev server

--- packages/app-client/vite.config.ts
+++ packages/app-client/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8787',
+      },
+    },
   },
   build: {
     target: 'esnext',
