78b8d38f2496cbf938a5e1c0095797df37103298
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2024-09-12 19:06:06 +0200

    feat(footer): added version number in footer (#119)

--- packages/app-client/src/modules/config/config.ts
+++ packages/app-client/src/modules/config/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   baseApiUrl: import.meta.env.VITE_BASE_API_URL ?? window.location.origin,
   documentationBaseUrl: import.meta.env.VITE_DOCUMENTATION_BASE_URL ?? 'https://docs.enclosed.cc',
+  enclosedVersion: import.meta.env.VITE_ENCLOSED_VERSION,
 };
