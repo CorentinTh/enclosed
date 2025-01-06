acb6ad8019ca293485f27c5a9b788c1971dcf130
mistersupri <supri.contact@gmail.com>
2025-01-06 13:03:39 +0000

    feat: install marked and highlight for markdown and syntax higlighting format note

---
+++ packages/app-client/src/modules/notes/pages/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'highlight.js';
+declare module 'highlight.js/lib/languages/javascript';
+declare module 'highlight.js/lib/languages/plaintext';
+declare module 'highlight.js/lib/languages/python';
+declare module 'highlight.js/lib/languages/bash';
+declare module 'highlight.js/lib/languages/stata';
+declare module 'highlight.js/lib/languages/stylus';
